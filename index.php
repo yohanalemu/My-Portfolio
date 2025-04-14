94d110b0fb442e8253f276fedacfb7cb56f00644
yohanalemu <149433169+yohanalemu@users.noreply.github.com>
2025-04-14 10:01:43 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="icon" type="image/png" href="favicon.png">
+<link rel="icon" href="./favicon.png" type="image/png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Portfolio</title>
